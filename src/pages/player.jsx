108da816f1a4a8570ced8acc2f354d8fa46439e2
Jedidiaz <jedidiazfagundez@gmail.com>
2022-12-16 20:11:27 -0500

    nuevas vistas

--- src/pages/player.jsx
+++ src/pages/player.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import Stepper from '@mui/material/Stepper';
+import "../styles/player.css";
+
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
 
 import player from "../assets/player.svg";
 
 const Player = () => {
-    let clases = []
-    for (let i = 0; i < 10; i++){
-        clases.push({
-            name: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit,',
-            completed: false
-        })
-    }
+  let clases = [];
+  for (let i = 0; i < 10; i++) {
+    clases.push({
+      name: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit,",
+      completed: false,
+    });
+  }
+
+  //steper
+  const [activeStep, setActiveStep] = React.useState(1);
   return (
     <>
       <div className="contenedor-player">
@@ -24,84 +31,104 @@ const Player = () => {
               <Form>
                 <div className="mb-3">
                   <Form.Check
-                    label="Payment methods"
-                    name="group1"
+                    label="Description"
+                    name="group3"
                     type="radio"
-                    id="Payment-methods"
+                    id="Description"
+                    checked
                   />
                   <Form.Check
-                    label="Purchase history"
-                    name="group1"
+                    label="Reviews"
+                    name="group3"
                     type="radio"
-                    id="Purchase-history"
+                    id="Reviews"
                   />
                   <Form.Check
-                    label="U credits"
-                    name="group1"
+                    label="learning tools"
+                    name="group3"
                     type="radio"
-                    id="U-credits"
+                    id="learning-tools"
                   />
                 </div>
               </Form>
             </div>
             <div className="description">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
-              erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-              tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-              consequat. Duis autem vel eum iriure dolor in hendrerit in
-              vulputate velit esse molestie consequat, vel illum dolore eu
-              feugiat nulla facilisis at vero eros et accumsan et iusto odio
-              dignissim qui blandit praesent luptatum zzril delenit augue duis
-              dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
-              consectetuer adipiscing elit, sed diam nonummy nibh euismod
-              tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi
-              enim ad minim veniam, quis nostrud exerci tation ullamcorper
-              suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis
-              autem vel eum iriure dolor in hendrerit in vulputate velit esse
-              molestie consequat, vel illum dolore eu feugiat nulla facilisis at
-              vero eros et accumsan et iusto odio dignissim qui blandit praesent
-              luptatum zzril delenit augue duis dolore te feugait nulla
-              facilisi. <br />
-              <br /> Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
-              sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-              aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud
-              exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
-              commodo consequat. Duis autem vel eum iriure dolor in hendrerit in
-              vulputate velit esse molestie consequat, vel illum dolore eu
-              feugiat nulla facilisis at vero eros et accumsan et iusto odio
-              dignissim qui blandit praesent luptatum zzril delenit augue duis
-              dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
-              consectetuer adipiscing elit, sed diam nonummy nibh euismod
-              tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi
-              enim ad minim veniam, quis nostrud exerci tation ullamcorper
-              suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis
-              autem vel eum iriure dolor in hendrerit in vulputate velit esse
-              molestie consequat, vel illum dolore eu feugiat nulla facilisis at
-              vero eros et accumsan et iusto odio dignissim qui blandit praesent
-              luptatum zzril delenit augue duis dolore te feugait nulla
-              facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing
-              elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-              magna aliquam erat volutpat. <br />
-              <br /> Ut wisi enim ad minim veniam, quis nostrud exerci tation
-              ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-              consequat. Duis autem vel eum iriure dolor in hendrerit in
-              vulputate velit esse molestie consequat, vel illum dolore eu
-              feugiat
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
+                in hendrerit in vulputate velit esse molestie consequat, vel
+                illum dolore eu feugiat nulla facilisis at vero eros et accumsan
+                et iusto odio dignissim qui blandit praesent luptatum zzril
+                delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
+                dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+                tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+                commodo consequat. Duis autem vel eum iriure dolor in hendrerit
+                in vulputate velit esse molestie consequat, vel illum dolore eu
+                feugiat nulla facilisis at vero eros et accumsan et iusto odio
+                dignissim qui blandit praesent luptatum zzril delenit augue duis
+                dolore te feugait nulla facilisi. <br />
+                <br /> Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+                nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+                aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
+                in hendrerit in vulputate velit esse molestie consequat, vel
+                illum dolore eu feugiat nulla facilisis at vero eros et accumsan
+                et iusto odio dignissim qui blandit praesent luptatum zzril
+                delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
+                dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+                tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+                commodo consequat. Duis autem vel eum iriure dolor in hendrerit
+                in vulputate velit esse molestie consequat, vel illum dolore eu
+                feugiat nulla facilisis at vero eros et accumsan et iusto odio
+                dignissim qui blandit praesent luptatum zzril delenit augue duis
+                dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,
+                consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                tincidunt ut laoreet dolore magna aliquam erat volutpat. <br />
+                <br /> Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                consequat. Duis autem vel eum iriure dolor in hendrerit in
+                vulputate velit esse molestie consequat, vel illum dolore eu
+                feugiat
+              </p>
             </div>
           </div>
         </div>
-        <div className="flow-class-player">
-          <div className="reports-payer">
+        <div className="follow-class-player">
+          <div className="reports-player">
             <h2>Course content</h2>
             <div>
-            <button type="button" className="btn btn-primary"><i className="bi bi-person-fill"></i> instrictor</button>
-            <button type="button" className="btn btn-primary"><i classname="bi bi-exclamation-circle"></i> instrictor</button>
+              <button type="button" className="btn btn-primary">
+                <i className="bi bi-person-fill"></i> instrictor
+              </button>
+              <button type="button" className="btn btn-primary">
+                <i className="bi bi-exclamation-circle"></i> Report a problem
+              </button>
             </div>
           </div>
           <div className="clases-player">
             <div className="info-clases">
-                
+              {clases.map((el) => (
+                <p>
+                  <span key={el.name}>{el.name}</span>
+                </p>
+              ))}
+            </div>
+            <div className="stepper-clases">
+              <Stepper activeStep={activeStep} orientation="vertical">
+                {clases.map(() => (
+                  <Step>
+                    <StepLabel></StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
             </div>
           </div>
         </div>
